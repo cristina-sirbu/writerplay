3c10f5dc91f37415f14c56ff608ca883fb814c48
Cristina Junca <sirbucristina@gmail.com>
2025-05-21 10:28:32 +0200

    Initial commit

---
+++ src/main/java/com/cristina/writerplay/WriterplayApplication.java
@@ -0,0 +1,13 @@
+package com.cristina.writerplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristina/writerplay/WriterplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristina.writerplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriterplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
